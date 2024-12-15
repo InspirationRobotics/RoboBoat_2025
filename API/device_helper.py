690757c9e2d42b6d91343ccba8824825894d3a7f
Leonard Wright <leonardalwright@gmail.com>
2024-12-15 15:37:14 -0800

    Build device_helper based on RoboSub

--- API/device_helper.py
+++ API/device_helper.py
@@ -25,15 +25,60 @@ def load_json(path):
 
 # Will have to differentiate when we get to a new ASV
 file_dir = os.path.dirname(os.path.abspath(__file__))
-settings = load_json(f"{file_dir}/../config/barco_polo.json")
+settings = load_json(f"{file_dir}/../Config/barco_polo.json")
 
-def findFromID(ids):
+def findFromId(ids):
     """
     Finding devices based on their port ID.
 
     Args:
-        ids (str): Port to find the device
+        ids (list): Port to find the device
     """
-    pass
+    print("Starting findFromId")
+
+    # Read script and split output by line
+    bash = os.popen("bash ~/RoboBoat_2025/API/usb_link.sh").read()
+    bashSplit = bash.split('\n')
+    result = []
+
+    # Iterate through passed ids and indices
+    for id in enumerate(ids):
+        result.append("")
+        # Iterate through lines in splitted bash output
+        for line in bashSplit:
+            # If line in /dev/tty
+            if "/dev/tty" in line:
+                line = line.split(" - ")
+                if id[1] in line[1]:
+                    result[id[0]] = line[0]
+    
+    # Remove empty spots if there aren't matching devices.
+    # If there's a result, return the device path
+    for i in reversed(result):
+        if i == "":
+            result.remove(i)
+    if len(result) == 0:
+        print(bash)
+        return ["Device not found, above is a list of all available devices"]
+    return result[0]
+
+def dataFromConfig(name):
+    """Get USB configuration from device based on name"""
+    # See: /Config/barco_polo.json
+    # use data for cameras, use usbID for USB cams or other USB connections
+    data = None
+    usbID = None
+    if name == "arduino_port":
+        usbID = settings.get("arduino_port")
+    else:
+        data = settings.get(name)
+        if data == None:
+            raise Exception("Invalid Name")
+    if data != None:
+        return data
+    if usbID == None:
+        print("id not found")
+        return None
+    return findFromId([usbID])
 
 # TODO: The rest of the file
