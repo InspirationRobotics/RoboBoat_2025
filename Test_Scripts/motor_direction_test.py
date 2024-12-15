454d915298efaec882729df2ebccdab867dd7344
Leonard Wright <leonardalwright@gmail.com>
2024-12-14 18:43:09 -0800

    Make motor tests

---
+++ Test_Scripts/motor_direction_test.py
@@ -0,0 +1,21 @@
+"""This is a motor direction test. It will run each of the motors at a moderate forward speed for 4 seconds each.
+It is meant to distinguish which argument maps to which thruster and the directionalities of the thrusters for forward
+power (forwards or backwards). This an in-water test!"""
+
+import time
+from API import t200
+
+# Double-check to see if the hardcoded port /tty/ACM0 is correct
+motors = t200.T200()
+
+# Run each thruster moderately forward for 4 secs
+motors.set_thrusters(0.5, 0, 0, 0)
+time.sleep(4)
+motors.set_thrusters(0, 0.5, 0, 0)
+time.sleep(4)
+motors.set_thrusters(0, 0, 0.5, 0)
+time.sleep(4)
+motors.set_thrusters(0, 0, 0, 0.5)
+time.sleep(4)
+
+motors.stop_thrusters()
---
+++ Test_Scripts/motor_direction_test_land.py
@@ -0,0 +1,21 @@
+"""This is a motor direction test. It will run each of the motors at a slow forward speed for 1.5 seconds each.
+It is meant to distinguish which argument maps to which thruster and the directionalities of the thrusters for forward
+power (forwards or backwards). This a land test!"""
+
+import time
+from API import t200
+
+# Double-check to see if the hardcoded port /tty/ACM0 is correct
+motors = t200.T200()
+
+# Run each thruster slowly forward for 1.5 secs
+motors.set_thrusters(0.16, 0, 0, 0)
+time.sleep(1.5)
+motors.set_thrusters(0, 0.16, 0, 0)
+time.sleep(1.5)
+motors.set_thrusters(0, 0, 0.16, 0)
+time.sleep(1.5)
+motors.set_thrusters(0, 0, 0, 0.16)
+time.sleep(1.5)
+
+motors.stop_thrusters()
