360b677d9b81146da717211c009c2dfc3f43ada6
CS50HarvardKC <networkmail.kc@gmail.com>
2024-12-29 17:46:55 -0500

    Updating .sh files. install_python stil does not work.

--- Setup/install_git.sh
+++ Setup/install_git.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Note that this file is not useful, since git will have to be installed before this repository can be cloned.
 
 echo "Beginning Git installation."
 sudo add-apt-repository -y ppa:git-core/ppa
--- Setup/install_python.sh
+++ Setup/install_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Bash script to install Python.
+# Bash script to install Python on Ubuntu, Debian or Red Hat-based systems.
 # Not tested yet.
 
 # Function to print in bold green text.
@@ -8,10 +8,10 @@ echo_msg(){
     echo -e "\033[1;32m$1\033[0m"
 }
 
-# Check if the user has sudo privileges.
-if [[$EUID -ne 0]]; then
+# Check if the script is run as root.
+if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root. Use sudo".
-    exit 
+    exit 1
 fi 
 
 # Function to check if a command exists.
@@ -19,42 +19,51 @@ command_exists(){
     command -v "$1" > /dev/null 2>&1
 }
 
+cd /
+pwd
+
 # Update package list.
 echo_msg "Updating package list..."
-apt-get update -y || (yum check-update && yum -y update)
-
-# Install dependencies.
 if command_exists apt-get; then
-    apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev \
-        libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget
-
+    sudo apt-get update -y
 elif command_exists yum; then
-    yum groupinstall -y "Development Tools"
-    yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel \
-        sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel wget
-
+    yum check-update && yum -y update
 else
     echo "Unsupported package manager. Please make sure you are using a Debian-based or Red Hat-based system." 
     exit 1
 fi
 
+# Install dependencies.
+echo_msg "Installing required dependencies..."
+if command_exists apt-get; then
+    sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev \
+        libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget
+elif command_exists yum; then
+    yum groupinstall -y "Development Tools"
+    yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel \
+        sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel wget
+fi
+
 read -p "Enter the Python version to install (e.g. 3.11.6): " PYTHON_VERSION
 
-if command_exists python3 && [["$(python3 --version)" == *"$PYTHON_VERSION"]]; then
+if command_exists python3 && [[ "$(python3 --version 2>/dev/null)" == *"$PYTHON_VERSION"* ]]; then
     echo_msg "Python $PYTHON_VERSION is already installed."
     exit 0
 fi
 
 # Download and install Python
 echo_msg "Downloading Python $PYTHON_VERSION..."
-wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz -0 /tmp/Python-$PYTHON_VERSION.tgz
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz -O /tmp/Python-$PYTHON_VERSION.tgz
+if [[ $? -ne 0 ]]; then
+    echo_msg "Failed to download Python source. Please check the version and try again."
+    exit 1
+fi
 
 echo "Extracting Python $PYTHON_VERSION..."
 cd /tmp && tar -xzf Python-$PYTHON_VERSION.tgz
 
 cd Python-$PYTHON_VERSION
 echo "Configuring Python $PYTHON_VERSION..."
-
 ./configure --enable-optimizations
 
 echo_msg "Building and installing $PYTHON_VERSION (this may take a while, please be patient)..."
@@ -62,17 +71,14 @@ make -j$(nproc)
 make altinstall
 
 # Verify installation
-if command_exists python3.$(echo $PYTHON_VERSION | cut -d. -f2); then
+echo "Verifying python installation..."
+PYTHON_MINOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f2)
+if command_exists python3.$PYTHON_MINOR_VERSION; then
     echo_msg "Python $PYTHON_VERSION installed successfully."
-    python3.$(echo $PYTHON_VERSION | cut -d. -f2) --version
+    python3.$PYTHON_MINOR_VERSION --version
 else
     echo_msg "Python installation failed."
     exit 1
 fi
 
-# Cleaning up
-echo "Cleaning up..."
-cd /tmp && rm -rf Python-$PYTHON_VERSION.tgz Python-$PYTHON_VERSION
-
 echo_msg "Python $PYTHON_VERSION installation is complete."
-
