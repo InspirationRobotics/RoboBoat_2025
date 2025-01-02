f12e7db7b2c0dc10eb92d4974559ae10f1257042
CS50HarvardKC <networkmail.kc@gmail.com>
2025-01-01 19:37:28 -0500

    Adding last changes to map visualizer Still does not work -- pivoting to something else

--- GNC/Nav_Core/map_visualizer.py
+++ GNC/Nav_Core/map_visualizer.py
@@ -15,7 +15,7 @@ Next Steps:
 
 class mapVisualizer(mapHelper):
     def __init__(self, /, map : list = [], scale : float = 1.0, save : bool = False, zoom : int = 16, frame_size : int = 800, *,
-                 path_to_save_image : str | Path = ''):
+                 path_to_save_image : str | Path = '', magnify : float = 1.0):
         super().__init__()
         self.loaded_map = map
         self.objects_to_draw = self.loaded_map
@@ -23,9 +23,10 @@ class mapVisualizer(mapHelper):
         self.map_scale = scale
         self.save = save
         self.zoom = zoom
-        self.frame_size = frame_size
+        # self.frame_size = frame_size
+        self.magnification_spec = magnify
 
-        self.get_map(save = save)
+        self.get_map(save = self.save)
         
         if path_to_save_image:
             self.image_path = path_to_save_image
@@ -99,27 +100,32 @@ class mapVisualizer(mapHelper):
     
     def rescale(self, x : int, y : int):
         size_w = self.map_obj.w
+        # print(f"Width of map : {size_w}")
         size_h = self.map_obj.h
-        x = int(x * self.frame_size / size_w)
-        y = int(y * self.frame_size / size_h)
+        # print(f"Height of map : {size_h}")
+        x = int(x * self.frame.shape[0]/ size_w)
+        y = int(y * self.frame.shape[1] / size_h)
         return x, y
         
     def get_map(self, /, save : bool = False):
-        self.scaled_corners = self.calculate_scaled_map_corners()
+        self.scaled_corners = self.parse_map_corners()
         lat_min = self.scaled_corners[0][0]
         lon_min = self.scaled_corners[0][1]
         lat_max = self.scaled_corners[3][0]
         lon_max = self.scaled_corners[3][1]
         zoom = self.zoom
-        map = smopy.Map((lat_min, lon_min, lat_max, lon_max), z=zoom)
+        map = smopy.Map((lat_min, lon_min, lat_max, lon_max), z=zoom, margin=.1)
         if save:
             if not isinstance(self.image_path, Path):
                 self.image_path = Path(self.image_path)
             map.save_png(f"{self.image_path}/map_visualized.png")
         self.map_obj = map
+        print(self.map_obj.xmin, self.map_obj.ymin)
         frame = cv2.cvtColor(map.to_numpy(), cv2.COLOR_RGB2BGR)
-        frame = cv2.resize(frame, (self.frame_size, self.frame_size), interpolation=cv2.INTER_LINEAR)
         # sharpen the frame
+        frame = cv2.resize(frame, (int(self.map_obj.w * self.magnification_spec), 
+                                   int(self.map_obj.h * self.magnification_spec)), 
+                                   interpolation=cv2.INTER_LINEAR)
         frame = cv2.GaussianBlur(frame, (0, 0), 1.0)
         frame = cv2.addWeighted(frame, 1.5, frame, -0.5, 0)
         kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
@@ -139,20 +145,12 @@ class mapVisualizer(mapHelper):
         print(f"Initial : {x, y}")
         print(f"Min: {min_x, min_y}")
         
-        # # Shift pixel scale
-        
-        
-        # # scale the x,y to the frame size
-        # x -= min_x
-        # y -= min_y
-        # print(f"Min: {min_x, min_y}")
-        # print(f"Debug: {x, y}")
-        # 
-        min_x, min_y = self.rescale(x, y)
+        # Shift pixel scale
+        min_x, min_y = self.rescale(min_x, min_y)
         print(f"Debug min rescale: {min_x, min_y}")
 
         frame = self.frame.copy()
-        frame = cv2.circle(frame, (min_x, min_y), 5, (0, 0, 255), -1)
+        # frame = cv2.circle(frame, (min_x, min_y), 5, (0, 0, 255), -1)
         cv2.imshow("map", frame)
         cv2.waitKey(0)
     
