d906389d5e5c03bfeed05ec570ddadd80455040d
Chase-520 <chace070528@gmail.com>
2025-02-09 14:54:43 -0800

    0209 different motor test with more terminal control

---
+++ Test_Scripts/API_Tests/Motor_Tests/contro_motor.py
@@ -0,0 +1,38 @@
+import time
+from API.Motors import t200
+motors = t200.T200(debug=True)
+
+
+
+while(True):
+    user_input = input("command>> ")
+    if(str(user_input) == "q"):
+        print("quiting the code")
+        break
+    elif(str(user_input) == "1"):
+        start = time.perf_counter_ns()
+        motors.set_thrusters(0.5, 0, 0, 0)
+        end = time.perf_counter_ns()
+        print(f"elapsed time: {end-start} ns")
+    elif(str(user_input) == "2"):
+        start = time.perf_counter_ns()
+        motors.set_thrusters(0, 0.5, 0, 0)
+        end = time.perf_counter_ns()
+        print(f"elapsed time: {end - start} ns")
+    elif(str(user_input) == "3"):
+        start = time.perf_counter_ns()
+        motors.set_thrusters(0, 0, 0.5, 0)
+        end = time.perf_counter_ns()
+        print(f"elapsed time: {end - start} ns")
+    elif(str(user_input) == "4"):
+        start = time.perf_counter_ns()
+        motors.set_thrusters(0, 0, 0, 0.5)
+        end = time.perf_counter_ns()
+        print(f"elapsed time: {end - start} ns")
+    elif(str(user_input) == "0"):
+        start = time.perf_counter_ns()
+        motors.stop_thrusters()
+        end = time.perf_counter_ns()
+        print(f"elapsed time: {end - start} ns")
+
+
