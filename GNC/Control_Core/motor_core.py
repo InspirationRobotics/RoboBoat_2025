6bd041234f3c200bdf4192e72bb0d06d81441a28
Chase-520 <chace070528@gmail.com>
2025-02-09 16:12:12 -0800

    update surge function

--- GNC/Control_Core/motor_core.py
+++ GNC/Control_Core/motor_core.py
@@ -28,7 +28,7 @@ class MotorCore():
     
     def surge(self, magnitude):
         """Configures for forward (positive magnitude) or backward (negative magnitude) movement"""
-        self.t200.set_thrusters(magnitude, -magnitude, -magnitude, magnitude)
+        self.t200.set_thrusters(-magnitude, -magnitude, magnitude, -magnitude)
 
     def sway(self, magnitude):
         """Configures for right (positive magnitude) or left (negative magnitude) movement"""
@@ -41,6 +41,8 @@ class MotorCore():
     """
     --------------------------------------------------------
     """
+
+
     def stay(self):
         """set all motors to 0"""
         self.t200.set_thrusters(0,0,0,0)
