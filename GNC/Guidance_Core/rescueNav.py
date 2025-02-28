77f0afa933b4cc0ecda822b541ecb77f667dd672
Chase-520 <chace070528@gmail.com>
2025-02-28 15:27:03 -0800

    basic structure for rescue mission

--- GNC/Guidance_Core/rescueNav.py
+++ GNC/Guidance_Core/rescueNav.py
@@ -13,6 +13,49 @@ class Rescue:
         self.info       = infoCore(modelPath=self.config["test_model_path"],labelMap=self.config["test_label_map"])
         self.motor      = MotorCore()
         self.wayPNav    = waypointNav()
-        self.servo = mini_maestro.MiniMaestro(self.servo_port)
+        self.servo      = mini_maestro.MiniMaestro(self.servo_port)
 
-    def 
+        self.cross      = False
+        self.triangle   = False
+
+        self.crossObject    = []
+        self.triangleObject = []
+
+        # TODO declare mini maestro channel for water gun and racketball launcher also the PWM value
+
+    def start(self):
+        self.info.start_collecting()
+
+    
+    def run(self):
+        on = True
+        while(on):
+            gps, detections = self.info.getInfo()
+            for object in detections:
+                label = object["label"]
+                if(label=="cross" or label=="black boat" and not self.cross):
+                    self.wayPNav.loadWaypoints([object["location"]])
+                    self.wayPNav.run()
+                    
+                    # code for water gun and racketball
+                    self.servo.set_pwm(self.racquetball_launcher_channel, self.launchPWM)
+                    time.sleep(2)
+                    self.servo.set_pwm(self.racquetball_launcher_channel, self.nominalPWM)
+                    self.cross = True
+
+                elif(label=="triangle"or label== "orange boat" and not self.triangle):
+                    self.wayPNav.loadWaypoints([object["location"]])
+                    self.wayPNav.run()
+
+                    # code for water gun and racketball
+                    self.servo.set_pwm(self.water_cannon_channel, self.launchPWM)
+                    time.sleep(2)
+                    self.servo.set_pwm(self.water_cannon_channel, self.nominalPWM)
+
+                    self.triangle = True
+                
+                    on = False # stop the while loop
+
+            self.motor.rotate(0.2) # rotate to find target
+
+        print("mission finished")
