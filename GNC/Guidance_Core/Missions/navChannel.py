5b47638986194dd5cb055876f47b1436f214ebbd
CS50HarvardKC <networkmail.kc@gmail.com>
2025-03-03 19:23:41 -0500

    Consistency across hosts

---
+++ GNC/Guidance_Core/Missions/navChannel.py
@@ -0,0 +1,11 @@
+from GNC.Nav_Core import gis_funcs
+
+class navChannel:
+    def __init__(self, *, infoCore, motors):
+        self.info = infoCore
+        self.motors = motors
+
+    def run(self):
+        pass
+
+
