73b86db984965554a7a36c48568c3e0eeb9e2e1a
Chase-520 <chace070528@gmail.com>
2025-03-05 16:12:35 -0500

    make it 10 s

--- GNC/Guidance_Core/waypointNav.py
+++ GNC/Guidance_Core/waypointNav.py
@@ -153,7 +153,7 @@ if __name__ == "__main__":
     maestro.set_pwm(1, 1500)  # Move servo on channel 1
     time.sleep(1)
     maestro.set_pwm(1, 1800)  # Move servo on channel 1   
-    time.sleep(5)
+    time.sleep(10)
     maestro.set_pwm(1, 1500)  # Move servo on channel 1
     time.sleep(1)
 
