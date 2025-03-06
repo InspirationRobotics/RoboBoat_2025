1bed8163eadfdb22c05cde9ad00e743f91a71677
CS50HarvardKC <networkmail.kc@gmail.com>
2025-03-06 12:16:20 -0500

    change model path

--- GNC/Guidance_Core/Missions/FTP.py
+++ GNC/Guidance_Core/Missions/FTP.py
@@ -105,12 +105,12 @@ class FTP:
         self.info.stop_collecting()
         print("Background Threads stopped")
         self.motors.stop()
-        print("Motors stoped")
+        print("Motors stopped")
 if __name__ == "__main__":
     config     = MissionHelper()
     print("loading configs")
     config     = config.load_json(path="GNC/Guidance_Core/Config/barco_polo.json")
-    info       = infoCore(modelPath=config["sign_model_path"],labelMap=config["sign_label_map"])
+    info       = infoCore(modelPath=config["competition_model_path"],labelMap=config["comptition_label_map"])
     print("start background threads")
     info.start_collecting()
     motor      = motor_core_new.MotorCore("/dev/ttyACM2") # load with default port "/dev/ttyACM2"
