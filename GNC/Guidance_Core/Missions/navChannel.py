9468b44491a407ce18930cebe48ef17774d66692
CS50HarvardKC <networkmail.kc@gmail.com>
2025-03-06 10:45:25 -0500

    Change params

--- GNC/Guidance_Core/Missions/navChannel.py
+++ GNC/Guidance_Core/Missions/navChannel.py
@@ -4,12 +4,12 @@ class navChannel:
     def __init__(self, *, infoCore, motors):
         self.info = infoCore
         self.motors = motors
-        self.distance = 25 # How far to move from the initial position (meters)
+        self.distance = 35 # How far to move from the initial position (meters)
 
     def run(self):
         currentGPSData, _ = self.info.getInfo()
         currLat, currLon, currHeading = (currentGPSData.lat, currentGPSData.lon, currentGPSData.heading)
-        calc_lat, calc_lon = gis_funcs.destination_point(currLat, currLon, currHeading, self.distance)
+        calc_lat, calc_lon = gis_funcs.destination_point(currLat, currLon, 270, self.distance)
         return calc_lat, calc_lon
     
         
