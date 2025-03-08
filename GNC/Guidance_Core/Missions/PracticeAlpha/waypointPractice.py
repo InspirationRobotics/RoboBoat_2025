0b035db59179220d5f0522cef16193bd5893ffb7
CS50HarvardKC <networkmail.kc@gmail.com>
2025-03-08 11:25:29 -0500

    CHnage

--- GNC/Guidance_Core/Missions/PracticeAlpha/waypointPractice.py
+++ GNC/Guidance_Core/Missions/PracticeAlpha/waypointPractice.py
@@ -32,7 +32,7 @@ tolerance = 1.5 # Meters
 #     print("[Mission] Waypoint reached.")
 
 waypoints  = NNAV._readLatLon(file_path = config["waypoint_file"])
-waypoints.insert(0,{"lat" : nav_lat, "lon" : nav_lon})
+# waypoints.insert(0,{"lat" : nav_lat, "lon" : nav_lon})
 
 # # Follow the path thread start
 # FTP = cvCore()
@@ -42,7 +42,7 @@ waypoints.insert(0,{"lat" : nav_lat, "lon" : nav_lon})
 # FTP_Thread.join()
 try:
     for index, p in enumerate(waypoints):
-        if(index==2):
+        if(index==1):
             pass
             Servo.set_pwm(1,1500)
             Servo.set_pwm(1,1800)
