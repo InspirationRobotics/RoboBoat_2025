21174f81ef12bb2c6dd9fae6650577de3dc2f203
colin-szeto <kolinszeto@gmail.com>
2025-04-21 18:46:51 -0700

    changed file name

--- GNC/Guidance_Core/Missions/blackCross.py
+++ GNC/Guidance_Core/Missions/blackCross.py
@@ -9,7 +9,7 @@ frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+out = cv2.VideoWriter('outputMe.mp4', fourcc, 20.0, (frame_width, frame_height))
 
 while True:
     ret, frame = cam.read()
