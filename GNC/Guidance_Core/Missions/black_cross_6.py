403cbda782e632117d03a3be59911fe4bc795081
colin-szeto <kolinszeto@gmail.com>
2025-05-14 12:25:18 -0700

    recoloring black detection text

--- GNC/Guidance_Core/Missions/black_cross_6.py
+++ GNC/Guidance_Core/Missions/black_cross_6.py
@@ -234,7 +234,7 @@ try:
                     if valid_depths.size > 0:
                             black_dist_m = np.nanmean(valid_depths) / 1000.0
                             cv2.putText(frame, f'{black_dist_m:.2f}m', (x + 10, y),
-                                       cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
+                                       cv2.FONT_HERSHEY_SIMPLEX, 2, (245, 66, 230), 5)
                     
                     # === Estimate and Display Depth for White Squares ===
                     for ((wc_x, wc_y), w, h) in white_info:
