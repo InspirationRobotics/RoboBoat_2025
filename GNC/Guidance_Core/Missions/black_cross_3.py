ce50604ec27c5c2854ade2cfd6e89b54be0b06d9
colin-szeto <kolinszeto@gmail.com>
2025-05-25 21:24:59 -0700

    bc3 white hsv

--- GNC/Guidance_Core/Missions/black_cross_3.py
+++ GNC/Guidance_Core/Missions/black_cross_3.py
@@ -22,7 +22,7 @@ LAUNCH_DISTANCE_THRESHOLD = 40
 # night time hello
 LOWER_BLACK = np.array([0, 0, 0])
 UPPER_BLACK = np.array([120, 255,   2])
-LOWER_WHITE = np.array([0, 0, 170])
+LOWER_WHITE = np.array([0, 0, 50])
 UPPER_WHITE = np.array([105, 17, 210]) #100  10 234
 KERNEL = np.ones((5, 5), np.uint8)
 
